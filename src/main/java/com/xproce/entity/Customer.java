7834d21645180b11110db4b5f416e2f720efd0f4
yakouuut <34114498+yakouuut@users.noreply.github.com>
2024-01-16 11:25:08 +0100

    commit

---
+++ src/main/java/com/xproce/entity/Customer.java
@@ -0,0 +1,59 @@
+package com.xproce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String pwd;
+    private String role;
+
+    public Customer(Long id, String email, String pwd, String role) {
+        this.id = id;
+        this.email = email;
+        this.pwd = pwd;
+        this.role = role;
+    }
+
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
